b20336137be3dc8638666e6fece3a83d5c42afe7
Nikita <nikita.bortych@gmail.com>
2019-06-12 11:56:10 +0200

    ready for data_big
    add data_big folder with big files

--- gan_loss.py
+++ gan_loss.py
@@ -93,6 +93,37 @@ def train_epoch(G, D, optim_G, optim_D, train_dataloader, device='cuda:0'):
     return np.mean(mean_loss_G), np.mean(mean_loss_D), np.mean(mean_psnr)
 
 
+def eval_epoch(G, D, test_dataloader, device='cuda:0'):
+    content_criterion = nn.MSELoss()
+    mean_loss_G = []
+    mean_loss_D = []
+    mean_psnr = []
+    criterion = nn.BCELoss()
+
+    for sample in test_dataloader:
+        lr = sample['x'].to(device).float()
+        hr = sample['y'].to(device).float()
+
+        ones = torch.ones((len(lr), 1)).to(device).float()
+        zeros = torch.zeros((len(lr), 1)).to(device).float()
+
+        # Train the discriminator.
+        D.eval()
+        G.eval()
+
+        sr = G(lr)
+        disc_sr = D(sr.detach())
+        disc_hr = D(hr)
+        loss = criterion(disc_hr, ones) + criterion(disc_sr, zeros)
+        mean_loss_D.append(loss.item())
+        content_loss = content_criterion(sr, hr)
+        loss_G = criterion(disc_sr, ones)
+        percept_loss = content_loss + loss_G
+        mean_psnr+=[10 * log10(1 / content_loss.item())]
+        mean_loss_G.append(percept_loss.item())
+    return np.mean(mean_loss_G), np.mean(mean_loss_D), np.mean(mean_psnr)
+
+
 def plot_samples(generator, dataloader, epoch, device='cuda:0'):
     """
     Plot a number of low- and high-resolution samples and the superresolution
@@ -176,19 +207,34 @@ def plot_samples(generator, dataloader, epoch, device='cuda:0'):
 
 
 def main():
+    #for plotting
+    plt.rcParams["figure.figsize"] = (10,10)
+
     device = torch.device(args.device)
-    #determining the input data parameters
-    if args.is_fk_data:
-        output_dim = [127,62]
-        filename_x='data_fk_25'
-        filename_y='data_fk_125'
+    #determining the input data parameters and source
+    if args.is_big_data:
+        directory = "Data_big/"
+        if args.is_fk_data:
+            output_dim = [127, 154]
+            filename_x='data_fk_20_big'
+            filename_y='data_fk_10_big'
+        else:
+            output_dim = [251,301]
+            filename_x='data_20_big'
+            filename_y='data_10_big'
     else:
-        output_dim = [251,121]
-        filename_x='data_25'
-        filename_y='data_125'
+        directory = "Data/"
+        if args.is_fk_data:
+            output_dim = [127,62]
+            filename_x='data_fk_25'
+            filename_y='data_fk_125'
+        else:
+            output_dim = [251,121]
+            filename_x='data_25'
+            filename_y='data_125'
 
     #getting the data set and standartising it
-    dataset = Data(filename_x = filename_x, filename_y = filename_y, transforms=transforms.Compose([
+    dataset = Data(filename_x = filename_x, filename_y = filename_y, directory = directory, transforms=transforms.Compose([
         transforms.ToTensor()
         #this is the actual statistic, not the 0,1
         #transforms.Normalize(torch.tensor(-4.4713e-07).float(), torch.tensor(0.1018).float())
@@ -199,8 +245,8 @@ def main():
     train_data, test_data = random_split(dataset, [train_size, test_size])
 
     #dataloaders
-    train_dataloader = DataLoader(train_data, batch_size = args.batch_size, drop_last=True)
-    test_dataloader = DataLoader(test_data, batch_size = 4)
+    train_dataloader = DataLoader(train_data, batch_size=args.batch_size, drop_last=True)
+    test_dataloader = DataLoader(test_data, batch_size = 2)
 
     # Init generator model.
     if args.model == "SRCNN":
@@ -215,17 +261,34 @@ def main():
     optim_G = optim.Adam(generator.parameters(), lr = args.lr)
     optim_D = optim.Adam(discriminator.parameters(), lr = args.lr)
 
+    scheduler_g = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optim_G, patience=args.scheduler_patience, verbose = True)
+    scheduler_d = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optim_D, patience=args.scheduler_patience, verbose = True)
 
 
 
     plot_G = []
     plot_D = []
+    plot_G_val = []
+    plot_D_val = []
 
     for epoch in range(args.n_epochs):
         loss_G, loss_D, mean_psnr = train_epoch(generator, discriminator, optim_G, optim_D, train_dataloader, device)
 
         # Report model performance.
         print(f"Epoch: {epoch}, G: {loss_G}, D: {loss_D}, PSNR: {mean_psnr}")
+
+        if epoch%args.eval_interval ==0:
+            loss_G_val, loss_D_val, mean_psnr_val = eval_epoch(generator, discriminator, test_dataloader, device)
+            print(f"Validation on epoch: {epoch}, G: {loss_G_val}, D: {loss_D_val}, PSNR: {mean_psnr_val}")
+            #schedule based on psnr or based on separate losses
+            is_psnr_step = False
+            if is_psnr_step:
+                scheduler_g.step(mean_psnr_val)
+                scheduler_d.step(mean_psnr_val)
+            else:
+                scheduler_g.step(loss_G_val)
+                scheduler_d.step(loss_D_val)
+
         if epoch%args.save_interval == 0:
             plot_samples(generator, test_dataloader, epoch)
 
@@ -238,28 +301,35 @@ def main():
     plt.legend()
     plt.savefig("images/gan_loss.png")
     plt.close()
+    torch.save(generator, 'generator')
+
 
 if  __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--n_epochs', type=int, default=1000,
                         help='number of epochs')
-    parser.add_argument('--batch_size', type=int, default=2,
+    parser.add_argument('--batch_size', type=int, default=16,
                         help='batch size')
     parser.add_argument('--lr', type=float, default=0.002,
                         help='learning rate')
     parser.add_argument('--latent_dim', type=int, default=64,
                         help='dimensionality of the latent space, only relevant for EDSR')
-    parser.add_argument('--num_res_blocks', type=int, default=16,
+    parser.add_argument('--num_res_blocks', type=int, default=8,
                         help='Number of resblocks in model, only relevant for EDSR')
     parser.add_argument('--model', type=str, default="EDSR",
                         help="Model type. EDSR or SRCNN")
+    parser.add_argument('--is_big_data', type=bool, default=True,
+                        help='Is big version data')
     parser.add_argument('--is_fk_data', type=bool, default=False,
                         help='Is fourier data')
     parser.add_argument('--test_percentage', type=float, default=0.1,
                         help='size of the test set')
     parser.add_argument('--save_interval', type=int, default=10,
                         help='save every SAVE_INTERVAL epochs')
+    parser.add_argument('--eval_interval', type=int, default=5,
+                        help='evaluate on test set ever eval_interval epochs')
     parser.add_argument('--device', type=str, default="cpu", help="Training device 'cpu' or 'cuda:0'")
+    parser.add_argument('--scheduler_patience', type=int, default="10", help="How many epochs of no improvement to consider Plateau")
 
     args = parser.parse_args()
 
